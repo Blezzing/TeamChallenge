6ed2f83171b368e98d2fafaeee36b2135df4faf9
Blezzing <mmhmaster@hotmail.com>
2015-11-02 23:29:22 +0100

    Vievport added and a mass of assets have been added.

--- core/src/com/blezzing/teamchallenge/SplashScreen.java
+++ core/src/com/blezzing/teamchallenge/SplashScreen.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by mmhma on 31-10-2015.
@@ -11,14 +14,28 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 public class SplashScreen extends ScreenAdapter {
     private Main game;
 
+    private Viewport viewport;
     private OrthographicCamera guiCamera;
 
     public SplashScreen(Main game)
     {
         this.game = game;
 
+
         guiCamera = new OrthographicCamera(1920,1080);
-        guiCamera.position.set(1920/2,1080/2,0/2);
+        guiCamera.position.set(guiCamera.viewportWidth/2,guiCamera.viewportHeight/2,0);
+        guiCamera.update();
+
+        viewport = new StretchViewport(100,100,guiCamera);
+        viewport.apply();
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        viewport.update(width, height);
     }
 
     public void render(float delta)
@@ -37,14 +54,13 @@ public class SplashScreen extends ScreenAdapter {
 
     public void draw()
     {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        guiCamera.update();
-
         game.batcher.setProjectionMatrix(guiCamera.combined);
+
         game.batcher.begin();
-        game.font.draw(game.batcher,"Splash!!",1920/2,1080/2);
+        game.font.draw(game.batcher,"Splash!!",500,500);
         game.batcher.end();
     }
 }
